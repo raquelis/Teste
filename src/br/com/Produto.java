f633ccafe3b042b4194c81c5cac63e9a9930f45c
raquelis <raquel.s29@aluno.ifsc.edu.br>
2023-05-09 16:25:19 -0300

    Criação do produto

--- src/br/com/Produto.java
+++ src/br/com/Produto.java
@@ -1,5 +1,44 @@
 package br.com;
 
 public class Produto {
-
+	private long codigo;
+	private String descricao;
+	private int quantidade;
+	private double valor;
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public String consultar(int codigo) {
+		String consulta = "";
+		
+		consulta += "\n --------------";
+		consulta += "\n Código: " + this.codigo;
+		consulta += "\n Descrição: " + this.descricao;
+		consulta += "\n Quantidade: " + this.quantidade;
+		consulta += "\n Valor: R$ " + this.valor;
+		consulta += "\n --------------";
+		return consulta;
+	}
 }
